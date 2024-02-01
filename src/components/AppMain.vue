a5dfec7a120fa8b0c47e15d12209a3a8542273d2
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-02-01 17:25:54 +0100

    add: appmain script

---
+++ src/components/AppMain.vue
@@ -0,0 +1,27 @@
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    data(){
+        return{
+            baseUrl: 'http://127.0.0.1:8000',
+            projects: []
+        };
+    },
+
+    created() {
+        axios.get(`${this.baseUrl}/api/projects`).then((resp) => {
+            this.projects = resp.data.results.data;
+        });
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
