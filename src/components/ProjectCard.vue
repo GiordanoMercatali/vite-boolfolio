a571a02597e4e46c5df36eb7672e11f0e0ce6314
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-02-01 19:10:31 +0100

    add: card

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,23 @@
+<template lang="">
+    <div class="card h-100">
+        <h4>{{ project.title }}</h4>
+        <p>{{ project.title }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        project: Object,
+    },
+    data(){
+        return{
+            serverUrl: 'http://127.0.0.1:8000',
+        };
+    },
+}
+</script>
+
+<style lang="">
+    
+</style>
