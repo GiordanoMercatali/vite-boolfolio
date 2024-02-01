a0167097fd5f014208fa77bd25f2f68d08d297c0
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-02-01 18:00:37 +0100

    add: export default to app

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
-<script setup>
-import axios from 'axios';
+<script>
+// import axios from 'axios';
 import AppMain from './components/AppMain.vue';
+
+export default {
+  components: {
+    AppMain,
+  },
+  data(){
+    return{
+    };
+  },  
+}
 </script>
 
 <template>
-  <AppMain/>
+  <h1>Test</h1>
+  <AppMain />
 </template>
 
 <style scoped>
-
 
 </style>
